bc50fc5bd7eb20e6fd15d63176df9115ecfb7664
柳沉翐 <593525228@qq.com>
2022-01-14 11:38:15 +0800

    20210112
    KMP review

--- SeasonLC/easy_28_KMP.cpp
+++ SeasonLC/easy_28_KMP.cpp
@@ -12,36 +12,68 @@ using namespace std;
 
 
 class Solution {
-public: // 在haystack（长）中找needle字符串！
 public:
     int strStr(string haystack, string needle) {
-        int n = haystack.size(), m = needle.size();
-        if (m == 0) {
+        // return haystack.find(needle);
+        // KMP算法
+        int len = needle.size();
+        if (!len) {
             return 0;
         }
-
-        vector<int> pi(m);
-
-        for (int i = 1, j = 0; i < m; i++) { // j是慢指针
-            while (j > 0 && needle[i] != needle[j]) {
-                j = pi[j - 1]; // 如果i≠j，
-            }
-            if (needle[i] == needle[j]) {
-                j++;
-            }
-            pi[i] = j;
+        int size = haystack.size();
+        if (!size) {
+            return -1;
         }
-        for (int i = 0, j = 0; i < n; i++) {
-            while (j > 0 && haystack[i] != needle[j]) {
-                j = pi[j - 1];
+
+
+        vector<int> next(len, -1); // next[]元素全都初始化为-1
+        // 真正的 next 数组会在此基础上每个元素 +1，并且 next 和字符串都是从脚标 1 开始存储
+        // 但这里都是按照从脚标 0 开始
+        if (len > 1) {
+            next[1] = 0;
+        }
+        int t = 0; // t 是模式串截至前一个字符的部分的最长公共前后缀长度（如果从 1 开始就是截至当前字符）
+
+
+        //生成next数组的核心代码==========================================================================
+        for (int k = 2; k < len; ++k) { // k为活动指针
+            if (needle[k - 1] == needle[t]) {
+                next[k] = ++t;
             }
-            if (haystack[i] == needle[j]) {
-                j++;
+            else {
+                while (needle[k - 1] != needle[t]) { // needle[k-1]的前几位，和needle[ next[t] ]的前几位相同。见Cache
+                    t = next[t]; // 找上一个needle[k]前缀一样的地方。
+                    if (t == -1) { // 已经回溯到模串的第一个字符了！！
+                        break;
+                    }
+                }
+                next[k] = ++t;
             }
-            if (j == m) {
-                return i - m + 1;
+        } // ↑ ↑ ↑ =======================================================================================
+
+
+        int mPtr = 0, patPtr = 0;
+        while (true) {
+            if (haystack[mPtr] == needle[patPtr]) {
+                if (patPtr == len - 1) {
+                    return mPtr - len + 1;
+                }
+                ++patPtr;
+                ++mPtr;
+            }
+            else {
+                if (next[patPtr] != -1) {
+                    patPtr = next[patPtr];
+                }
+                else {
+                    patPtr = 0;
+                    ++mPtr;
+                }
+            }
+            if (mPtr >= size || size - mPtr < len - patPtr) {
+                return -1;
             }
         }
-        return -1;
+        return 0;
     }
 };
