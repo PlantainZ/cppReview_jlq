2cce52cdba308e61b038771a928fee412a58f1be
柳沉翐 <593525228@qq.com>
2022-01-20 14:25:52 +0800

    2022-01-19
    medium review x 2

--- SeasonLC/medium_02.cpp
+++ SeasonLC/medium_02.cpp
@@ -6,10 +6,45 @@
 #include<minmax.h>
 using namespace std;
 
-/*
+/* 02.两数之和。见LC_Cache
 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
-
 请你将两个数相加，并以相同形式返回一个表示和的链表。
-
 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
 */
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+ListNode* addTwoNums(ListNode* l1, ListNode* l2) {
+    ListNode* head = new ListNode(-1); // 表头为空
+    ListNode* h = head; // 指引存数的地址
+
+    int sum = 0;
+    bool carry = false;
+
+    while (l1 != NULL || l2 != NULL) {
+        sum = 0;
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+        if (carry) sum++;
+        h->next = new ListNode(sum % 10);
+        h = h->next;
+        carry = sum >= 10 ? true : false;
+    }
+    if (carry) {
+        h->next = new ListNode(1);
+        h = h->next;
+    }
+    return head->next;
+}
---
+++ SeasonLC/medium_03.cpp
@@ -0,0 +1,98 @@
+﻿#include<stdio.h>
+#include<iostream>
+#include<unordered_map>
+#include<string>
+#include<stack>
+#include<minmax.h>
+using namespace std;
+
+// 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+
+/* 举例：
+输入: s = "abcabcbb"
+输出: 3
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+
+输入: s = "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+
+输入: s = "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+*/
+
+
+// 1. 滑动窗口 =====================================================
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        //定义两个游标,还有长度,与结果,均初始化为0
+        int start(0), end(0), length(0), result(0);
+
+        //只要end<字符串大小
+        while (end < s.size())
+        {
+            //该注释错误		//暂存首个字符char,
+            //其实应该是暂存end所指向的那个值,到时候会跟index所指向的值比较
+            char tempChar = s[end];
+
+            //定义index,它只用于改变start的指向一般为index+1
+            //因为index所处的与end的值相等,则没有统计的必要了
+            //for循环只用来判断是否相等
+            //
+            for (int index = start; index < end; index++) // 当index == end = 0时，不进行此循环。
+            {
+                //判断是否出现重复字符串
+                if (tempChar == s[index])
+                {
+                    // 如果出现,则将start跳过重复字符index指向的下一字符
+                    start = index + 1;
+                    // 保存长度
+                    length = end - start;
+
+                    // 如果相等,判断结束
+                    break;
+                }
+            }
+            //此时,end向后移动,每一一次,长度+1
+            end++;
+            length++;
+            //返回更新后的length,如果更大,则为它
+            result = max(result, length);
+        }
+        return result;
+    }
+};
+
+
+// 2.利用hashmap优化 =============================================================
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        //s[start,end) 前面包含 后面不包含
+        int start(0), end(0), length(0), result(0);
+        int sSize = int(s.size());
+        unordered_map<char, int> hash;
+
+        while (end < sSize)
+        {
+            char tmpChar = s[end];
+            //仅当s[start,end) 中存在s[end]时更新start
+            if (hash.find(tmpChar) != hash.end() && hash[tmpChar] >= start)
+            {
+                start = hash[tmpChar] + 1;
+                length = end - start;
+            }
+            hash[tmpChar] = end;
+
+            end++;
+            length++;
+            result = max(result, length);
+        }
+        return result;
+    }
+};
