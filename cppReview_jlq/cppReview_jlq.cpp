e5c586ac7c0e8f2046f3688d53ad715ba8f6a1d0
柳沉翐 <593525228@qq.com>
2022-01-21 20:39:44 +0800

    2020-01-21
    sth del.

--- cppReview_jlq/cppReview_jlq.cpp
+++ cppReview_jlq/cppReview_jlq.cpp
@@ -6,33 +6,12 @@
 
 int main()
 {
-    //std::cout << "This is the playground for jlq's algorithm system of brain.~\n";
-    //std::cout << "U R WHAT U REPEAT.\n";
-    //std::cout << "CPP IS THE BEST LANGUAGE OF THINGS WHICH CAN MANIPULATE CPU & MEMORY!!!\n";
-    //getchar();
-    //return 0; // github push test
-    //// github pull test
-
-    // 苍云 龙莲13989858545 367592zhangji
-    // 鲸鱼 墨夜无常15563791743 367592zhangji
-    std::cout << "890045/ sc:023 \n";
-    std::cout << "const['龙莲'] && const['墨夜无常']：True";
-
-    std::cout << "“龙莲”近似匹配结果：True";
-    std::cout << "[525698,7,11,43,&out,0,15246211437,...,1853,...,200],";
-    std::cout << "[4581,3,11,14,&out,1,Syyuo_0723,...,691,...,200],";
-    std::cout << "[37869,16,11,77,&out,1,611kekeaiai,...,2528,...,200],";
-    std::cout << "[685,92,11,485,&out,0,13989858545,...,22,...,200],";
-    std::cout << "[12698,13,11,39,&out,1,caillomml,...,79633,...,200]";
-    std::cout << "spandTime:%d s";
-    //std::cout << "“墨夜无常”近似匹配结果：True,268994/16/101/33/&out/15563791743/367592zhangji/6638/..."
     getchar();
     return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
-
 // 入门使用技巧: 
 //   1. 使用解决方案资源管理器窗口添加/管理文件
 //   2. 使用团队资源管理器窗口连接到源代码管理
